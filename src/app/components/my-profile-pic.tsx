a35c891a63de4205df49e90a7922dfc511d48625
Kelley Sharp <kelleylsharp@gmail.com>
2023-07-26 19:09:42 -0400

    try another next-cloudinary fix

--- src/app/components/my-profile-pic.tsx
+++ src/app/components/my-profile-pic.tsx
@@ -4,13 +4,7 @@ import { CldImage } from "next-cloudinary";
 export function MyProfilePic() {
   return (
     <section className="border-b-2 border-b-pink-500 md:border-none">
-      <CldImage
-        src="blog/afsqeub2vc0rvfvkar56"
-        width="500"
-        height="500"
-        alt="Kelley Sharp"
-        quality="100"
-      />
+      <CldImage src="blog/afsqeub2vc0rvfvkar56" width="500" height="500" alt="Kelley Sharp" />
     </section>
   );
 }
