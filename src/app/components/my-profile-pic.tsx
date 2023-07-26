0695da854aa84c31781ac371c2e4331af2713501
Kelley Sharp <kelleylsharp@gmail.com>
2023-07-26 19:33:44 -0400

    trying delivery type fetch

--- src/app/components/my-profile-pic.tsx
+++ src/app/components/my-profile-pic.tsx
@@ -4,7 +4,13 @@ import { CldImage } from "next-cloudinary";
 export function MyProfilePic() {
   return (
     <section className="border-b-2 border-b-pink-500 md:border-none">
-      <CldImage src="blog/afsqeub2vc0rvfvkar56" width="500" height="500" alt="Kelley Sharp" />
+      <CldImage
+        src="blog/afsqeub2vc0rvfvkar56"
+        width="500"
+        height="500"
+        alt="Kelley Sharp"
+        deliveryType="fetch"
+      />
     </section>
   );
 }
