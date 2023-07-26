1bdf3fc2ecff54ec20910467fd32511f1e0f4302
Kelley Sharp <kelleylsharp@gmail.com>
2023-07-26 18:23:06 -0400

    add cloudinary domain to next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["res.cloudinary.com"],
+    loader: "cloudinary",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
