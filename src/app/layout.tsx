076fb9647cb69811daf1d120a63a6139076eac15
Kelley Sharp <kelleylsharp@gmail.com>
2023-08-01 18:21:12 -0400

    word change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Kelley's Blog",
-  description: "Musings from a Software Engineer with 5 years of experience",
+  description: "Musings from a Software Engineer with over 5 years of experience",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
