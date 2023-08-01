94eb8353a1ea84a857f6dcc66dbdc3d4b9233a75
Kelley Sharp <kelleylsharp@gmail.com>
2023-08-01 14:34:56 -0400

    add og image file to post

--- src/app/posts/[postId]/opengraph-image.tsx
+++ src/app/posts/[postId]/opengraph-image.tsx
@@ -1,46 +1,44 @@
+import { CldImage } from "next-cloudinary";
 import { ImageResponse } from "next/server";
-
-// Route segment config
-export const runtime = "edge";
 
 export const contentType = "image/png";
 
-// Font
-// const interSemiBold = fetch(new URL("./Inter-SemiBold.ttf", import.meta.url)).then((res) =>
-//   res.arrayBuffer(),
-// );
-
-//image
-
 // Image generation
-export default async function Image({ params: { postId } }: { params: { postId: string } }) {
-  const imageUrl = await getImageByPostId({ postId: `${postId}/images/opengraph-image.png` });
+export default async function GET(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url);
 
-  return new ImageResponse(
-    (
-      // ImageResponse JSX element
-      <div
-        style={{
-          fontSize: 128,
-          background: "white",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        {/* Cloudinary Image Goes Here */}
-      </div>
-    ),
-    // ImageResponse options
-    {
-      width: 1200,
-      height: 630,
-    },
-  );
-}
+    const hasPostId = searchParams.has("postId");
+    const postId = hasPostId ? searchParams.get("postId")?.slice(0, 100) : "Kelley's Blog";
 
-async function getImageByPostId({ postId }: { postId: string }): Promise<string> {
-  // fetch here
+    return new ImageResponse(
+      (
+        // ImageResponse JSX element
+        <div
+          style={{
+            fontSize: 128,
+            background: "white",
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <CldImage alt={postId ? postId : "kelley's Blog"} src={`blog/${postId}`} />
+        </div>
+      ),
+      // ImageResponse options
+      {
+        width: 1200,
+        height: 630,
+      },
+    );
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    console.log(`${e.message}`);
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    });
+  }
 }
