052ee4e7b471f0db530ca08e2e0726294e12d0b0
Kelley Sharp <kelleylsharp@gmail.com>
2023-08-08 14:46:44 -0400

    fix logo res

--- src/app/components/my-logo.tsx
+++ src/app/components/my-logo.tsx
@@ -4,12 +4,13 @@ import { CldImage } from "next-cloudinary";
 type MyLogoProps = { className?: string; size?: number };
 export function MyLogo({ className, size }: MyLogoProps) {
   return (
-    <section className={className}>
+    <section style={{ width: 300 }} className={className}>
       <CldImage
         src="blog/oqdshwtaox54akkevzjw"
-        width={size ? size : 300}
-        height={size ? size : 300}
+        width={size ? size : 600}
+        height={size ? size : 339}
         alt="Kelley Sharp's Logo"
+        quality={100}
       />
     </section>
   );
